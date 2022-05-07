53f54c908017829e66288760d312b6dcb7ae915d
Meta39 <53995537@qq.com>
2022-05-07 19:08:11 +0800

    首次提交

---
+++ src/main/java/com/fu/rabbitmqssm/RabbitmqSsmApplication.java
@@ -0,0 +1,13 @@
+package com.fu.rabbitmqssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSsmApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/fu/rabbitmqssm/controller/SendMsgController.java
@@ -0,0 +1,40 @@
+package com.fu.rabbitmqssm.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/ttl")
+public class SendMsgController {
+    private static final Logger log = LoggerFactory.getLogger(SendMsgController.class);
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    //开始发消息
+    @GetMapping("/sendMsg/{message}")
+    public void sendMsg(@PathVariable String message){
+        log.info("当前时间：{},发送一条消息给2个TTL队列：{}",new Date().toString(),message);
+        rabbitTemplate.convertAndSend("X","XA","消息来自ttl为10S的队列："+message);
+        rabbitTemplate.convertAndSend("X","XB","消息来自ttl为40S的队列："+message);
+    }
+
+    //开始发消息 消息  ttl
+    @GetMapping("/sendExpirationMsg/{message}/{ttlTime}")
+    public void sendMsg(@PathVariable String message,@PathVariable String ttlTime){
+        log.info("当前时间：{},发送一条时长{}ms TTL消息给队列QC：{}",new Date().toString(),ttlTime,message);
+        rabbitTemplate.convertAndSend("X","XC",message,msg->{
+            //发送消息的时候   延迟时长
+            msg.getMessageProperties().setExpiration(ttlTime);
+            return msg;
+        });
+    }
+}
